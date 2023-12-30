52c432404672cadff4d0a58a85ea2428e7d3f1e3
Eduardo Bortolini <ebortolini@gmail.com>
2023-12-29 23:09:58 -0300

    Add Weighte dQuick Union with Path Compression

--- QuickUnion/Python/quick_union.py
+++ QuickUnion/Python/quick_union.py
@@ -18,3 +18,43 @@ class QuickUnionUF:
         id_p = self._get_root(p)
         id_q = self._get_root(q)
         self.ids[id_p] = id_q
+
+#reference: https://algs4.cs.princeton.edu/15uf/WeightedQuickUnionPathCompressionUF.java.html
+class WeightedQuickUnionPathCompressionUF:
+    def __init__(self, size:int) -> None:
+        self.parent = []
+        self.size = []
+        for n in range(size):
+            self.parent.append(n)
+            self.size.append(1)
+    
+    def _get_root(self, p:int) -> int:
+        while self.parent[p] != p:
+            p = self.parent[p]
+        return p
+    
+    def _compress_path(self, p:int, root:int) -> None:
+        while p != root:
+            new_p = self.parent[p]
+            self.parent[p] = root
+            p = new_p
+    
+    def connected(self, p:int, q:int) -> bool:
+        return self._get_root(p) == self._get_root(q)
+    
+
+    def union(self, p:int, q:int) -> None:
+        root_p = self._get_root(p)
+        self._compress_path(p, root_p)
+        root_q = self._get_root(q)
+        self._compress_path(q, root_q)
+
+        if root_p == root_q:
+            return
+        
+        if self.size[root_p] < self.size[root_q]:
+            self.parent[root_p] = root_q
+            self.size[root_p]+= self.size[root_q]
+        else:
+            self.parent[root_q] = root_p
+            self.size[root_q]+= self.size[root_p]
